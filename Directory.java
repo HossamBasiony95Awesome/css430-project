a686eee558885f6beef67ac1ed8f27a2e1a1e65e
Whompithian <bps7@uw.edu>
2012-12-09 21:35:47 -0800

    Comments and error checks in SuperBlock and Directory.

--- Directory.java
+++ Directory.java
@@ -1,8 +1,9 @@
 /*
  * @file    Directory.java
- * @brief   This class is the directory structure for a simple filesystem.
+ * @brief   This class is the directory structure for a simple filesystem. It
+ *           holds the names of every file, indexed by their inode numbers.
  * @author  Brendan Sweeney, SID 1161836
- * @date    December 12, 2012
+ * @date    December 14, 2012
  */
 public class Directory {
     private static int maxChars = 30;   // max characters of each file name
@@ -13,10 +14,11 @@ public class Directory {
 
     
     /**
-     * .
-     * @param  maxInumber  .
-     * @pre    .
-     * @post   .
+     * Instantiates this Directory to support a given number of files.
+     * @param  maxInumber  The number of files this Directory should support.
+     * @pre    The calling class has provided an appropriate maxInumber value.
+     * @post   This Directory can support the specified number of files, which
+     *          includes itself at index 0.
      */
     public Directory(int maxInumber) {  // directory constructor
         fsizes = new int[maxInumber];   // maxInumber = max files
@@ -30,14 +32,16 @@ public class Directory {
 
     
     /**
-     * .
-     * @param  data  .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Initializes this Directory from a byte array that was previously created
+     *  by directory2bytes(), assuming the same inode count.
+     * @param  data  A byte array that describes a directory structure.
+     * @pre    data supports the same number of files as this Directory.
+     * @post   This Directory is a duplicate of the state of the Directory that
+     *          created data.
+     * @return The number of entries read from data.
      */
     public int bytes2directory(byte data[]) {
-        int offset  = maxChars * 2 + 4,         // i offset for entries
+        int offset  = maxChars * 2 + 4,         // byte offset for each entry
             current,            // counter for the current entry to read
             entries = data.length / offset;     // number of entries to read
         // invalidate existing file entries
@@ -61,10 +65,12 @@ public class Directory {
 
     
     /**
-     * .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Stores the contents of this directory in a byte array. Each file entry
+     *  consumes 64 bytes in the array.
+     * @pre    None.
+     * @post   This Directory remains unchanged.
+     * @return A byte array, suitable for storing to disk, that represents the
+     *          file entries of this Directory.
      */
     public byte[] directory2bytes() {
         int offset  = maxChars * 2 + 4,     // entry offset marker
@@ -103,50 +109,60 @@ public class Directory {
 
     
     /**
-     * .
-     * @param  filename  .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Allocates the first unused inode number to a new file name by filename.
+     * @param  filename  The name of the file to create.
+     * @pre    filename does not name a file that exists in this Directory.
+     * @post   This Directory contains a file entry for a file with the name
+     *          specified by filename; an unused inode exists.
+     * @return The inode number associated with the new file if it was created;
+     *          -1 otherwise.
      */
     public short ialloc(String filename) {
         // filename is the one of a file to be created.
         // allocates a new inode number for this filename
-        for (short i = 0; i < fsizes.length; ++i) {
-            if (fsizes[i] == 0) {
-                fsizes[i] = filename.length();
-                filename.getChars(0, fsizes[i], fnames[i], 0);
-                return i;
-            } // end if (fsizes[i] == 0)
-        } // end for (; i < fsizes.length; )
-        
+        if (namei(filename) != -1) {
+            for (short i = 0; i < fsizes.length; ++i) {
+                if (fsizes[i] == 0) {
+                    fsizes[i] = filename.length();
+                    filename.getChars(0, fsizes[i], fnames[i], 0);
+                    return i;
+                } // end if (fsizes[i] == 0)
+            } // end for (; i < fsizes.length; )
+        } // end if (namei(filename) != -1)
+        // file name already in use or no inodes available
         return -1;
     } // end ialloc(String)
 
     
     /**
-     * .
-     * @param  iNumber  .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Frees the inode of a file so that it may be deleted.
+     * @param  iNumber  The inode number of the file to be deleted.
+     * @pre    iNumber is within range of the total inodes.
+     * @post   The specified inode is unregistered and its file entry lost.
+     * @return true if the specified inode pointed to a file in this Directory;
+     *          false otherwise.
      */
     public boolean ifree(short iNumber) {
-        boolean found = fsizes[iNumber] > 0;
+        boolean found = false;
         // deallocates this inumber (inode number)
         // the corresponding file will be deleted.
-        fsizes[iNumber] = 0;
+        if (iNumber > 0 && iNumber < fsizes.length) {
+            found = fsizes[iNumber] > 0;
+            fsizes[iNumber] = 0;
+        } // end if (iNumber > 0...)
         
         return found;
     } // end ifree(short)
 
     
     /**
-     * .
-     * @param  filename  .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Provides the inode number of the file specified by filename.
+     * @param  filename  The name of the file to locate.
+     * @pre    filename specifies the name of a file that exists in this
+     *          Directory.
+     * @post   This Directory remains unchanged.
+     * @return The inode number of the file specified by filename if such a
+     *          file could be found; -1 otherwise.
      */
     public short namei(String filename) {
         String test;
@@ -160,7 +176,7 @@ public class Directory {
                 } // end if (filename.compareTo(test) == 0)
             } // end if (fsizes[i] > 0)
         } // end for (; i < fsizes.length; )
-        
+        // no entry found with name specified by filename
         return -1;
     } // end namei(String)
 } // end class Directory
--- SuperBlock.java
+++ SuperBlock.java
@@ -1,8 +1,12 @@
 /*
  * @file    SuperBlock.java
- * @brief   .
+ * @brief   This class is a superblock for a virtual hard disk. It only needs
+ *           to maintain three variables: total blocks that make up the disk,
+ *           total inodes in the file system (maximum number of files), and the
+ *           first unused disk block. Despite the small size, the superblock
+ *           requires an entire block (block 0) when stored to disk.
  * @author  Brendan Sweeney, SID 1161836
- * @date    December 12, 2012
+ * @date    December 14, 2012
  */
 public class SuperBlock {
     private final int defaultTotalInodes = 64;
@@ -12,11 +16,17 @@ public class SuperBlock {
     
     
     /**
-     * .
-     * @param  files  .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Instantiates a SuperBlock with a given number of disk blocks. If an
+     *  existing superblock on disk exists with the specified block count, then
+     *  that superblock shall provide the remaining values for this one.
+     *  Otherwise, the disk is formatted to allot the proper blocks and the
+     *  remaining member variables are temporarily set to default values.
+     * @param  diskBlocks  The number of data blocks on the containing disk.
+     * @pre    diskBlocks is a positive number; if a format is needed, then the
+     *          calling class will set totalInodes and freeList to appropriate
+     *          values when it calls SuperBlock.format().
+     * @post   Block 0 of the virtual disk contains a superblock which is
+     *          represented by this one.
      */
     public SuperBlock(int diskBlocks) {
         byte[] superBlock = new byte[Disk.blockSize];
@@ -35,9 +45,10 @@ public class SuperBlock {
     
     
     /**
-     * .
-     * @pre    .
-     * @post   .
+     * Writes this SuperBlock to block 0 of the virtual disk.
+     * @pre    None.
+     * @post   Block 0 of the virtual disk contains a superblock which is
+     *          represented by this one.
      */
     public void sync() {
     /* write totalBlocks, inodeBlocks, freelist
@@ -51,9 +62,16 @@ public class SuperBlock {
     
 
     /**
-     * .
-     * @pre    .
-     * @post   .
+     * Formats a virtual disk to contain only a superblock and a free list. The
+     *  free list is a linked list of all unused disk blocks, where each block
+     *  in the list contains, in its first few bytes, the number of the next
+     *  block in the list. The number of the first block in this list is held
+     *  by freeList.
+     * @pre    numBlocks is a positive number; the calling class has set
+     *          totalInodes and freeList to appropriate values.
+     * @post   The virtual disk contains only a superblock in block 0, which is
+     *          represented by this one, and a sequence of unused blocks which
+     *          each contain the number of the following block.
      */
     public void format() {
         format(FileSystem.DEFAULT_BLOCKS);
@@ -61,15 +79,23 @@ public class SuperBlock {
     
     
     /**
-     * .
-     * @param  numBlocks  .
-     * @pre    .
-     * @post   .
+     * Formats a virtual disk to contain only a superblock and a free list. The
+     *  free list is a linked list of all unused disk blocks, where each block
+     *  in the list contains, in its first few bytes, the number of the next
+     *  block in the list. The number of the first block in this list is held
+     *  by freeList.
+     * @param  numBlocks  The number of data blocks on the containing disk.
+     * @pre    numBlocks is a positive number; the calling class has set
+     *          totalInodes and freeList to appropriate values.
+     * @post   The virtual disk contains only a superblock in block 0, which is
+     *          represented by this one, and a sequence of unused blocks which
+     *          each contain the number of the following block.
      */
     public void format(int numBlocks) {
         totalBlocks   = numBlocks;
         byte[] buffer = new byte[Disk.blockSize];
         
+        // initialize free list and write it to disk
         for (int i = freeList; i < totalBlocks - 1; ++i) {
             SysLib.int2bytes(i + 1, buffer, 0);
             SysLib.rawwrite(i, buffer);
@@ -77,37 +103,51 @@ public class SuperBlock {
         
         SysLib.int2bytes(-1, buffer, 0);
         SysLib.rawwrite(totalBlocks - 1, buffer);
-        sync();
+        sync();     // write superblock to disk
     } // end format(int)
     
 
     /**
-     * .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Provides the number of the first unused block on disk and removes that
+     *  block from the head of the free list.
+     * @pre    The free list contains an unused block.
+     * @post   freeList points to the block following the previous head of the
+     *          list, or contains -1 if the list is now empty.
+     * @return The number of the first unused block if one is available; -1
+     *          otherwise.
      */
     public int getFreeBlock() {
     /* dequeue top block 
         in freelist */
         byte[] buffer = new byte[Disk.blockSize];
-        int temp = freeList;
-        SysLib.rawread(freeList, buffer);
-        freeList = SysLib.bytes2int(buffer, 0);
+        int    temp   = freeList;
+        
+        if (freeList != -1) {
+            SysLib.rawread(freeList, buffer);
+            freeList = SysLib.bytes2int(buffer, 0);
+        } // end if (freeList != -1)
+        
         return temp;
     } // end getFreeBlock()
     
 
     /**
-     * .
-     * @param  oldBlockNumber  .
-     * @pre    .
-     * @post   .
-     * @return .
+     * Returns a block to the free list at the head.
+     * @param  oldBlockNumber  Number of the block to be placed at the head of
+     *                          the free list.
+     * @pre    oldBlockNumber is not already in the free list.
+     * @post   oldBlockNumber sits at the head of the free list and points to
+     *          the previous head.
+     * @return true of the specified block could be written to the free list;
+     *          false otherwise.
      */
     public boolean returnBlock(int oldBlockNumber) {
     /* enqueue oldBlockNumber 
         to top of freelist */
+        if (oldBlockNumber < 1 || oldBlockNumber > totalBlocks) {
+            return false;
+        } // end if (oldBlockNumber < 1...)
+        
         byte[] buffer = new byte[Disk.blockSize];
         SysLib.int2bytes(freeList, buffer, 0);
         freeList = oldBlockNumber;
